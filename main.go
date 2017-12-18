b590fb2ce9030f77fb833e91b1829773f98481ac
drmfly <contact@idrmfly.com>
2017-12-18 13:14:38 +0800

    support sub pkg,like qtrx.io/x/y/z

--- main.go
+++ main.go
@@ -17,24 +17,29 @@ func main() {
 		c.Redirect(301, QTraderURL)
 		return nil
 	})
-	e.GET("/:pkg", func(c echo.Context) error {
-		// Package name from path `/:pkg`
-		pkg := c.Param("pkg")
-		isGoGet := c.QueryParam("go-get")
-		if pkg != "" && isGoGet == "1" {
-			htmlstring := `<html>
+	//support qtrx.io/x,qtrx.io/x/y,qtrx.io/x/y/z, qtrx.io/x/y/z/
+	e.GET("/:pkg", handPkg)
+	e.GET("/:pkg/:subPkg", handPkg)
+	e.GET("/:pkg/:subPkg/:sSubPkg", handPkg)
+	e.GET("/:pkg/:subPkg/:sSubPkg/:sSSubPkg", handPkg)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func handPkg(c echo.Context) error {
+	pkg := c.Param("pkg")
+	isGoGet := c.QueryParam("go-get")
+	if pkg != "" && isGoGet == "1" {
+		htmlstring := `<html>
 	<head>
         <meta name="go-import" content="qtrx.io/` + pkg + ` git https://github.com/q-trader/` + pkg + `">
         <meta name="go-source" content="qtrx.io/` + pkg + `     https://github.com/q-trader/` + pkg + ` https://github.com/q-trader/` + pkg + `/tree/master{/dir} https://github.com/q-trader/` + pkg + `/blob/master{/dir}/{file}#L{line}">
     </head>
 </html>
 `
-			return c.HTML(http.StatusOK, htmlstring)
+		return c.HTML(http.StatusOK, htmlstring)
 
-		}
-		c.Redirect(301, QTraderURL)
-		return nil
-
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	}
+	c.Redirect(301, QTraderURL)
+	return nil
 }
